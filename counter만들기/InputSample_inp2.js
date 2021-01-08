7429be29a787adca8dd596acfe8ab4dcf088596d
K_bora14 <36124197+kwakbora@users.noreply.github.com>
2021-01-08 14:01:18 +0900

    Update InputSample_inp2.js
    useSate useRef 의 활용

--- counter만들기/InputSample_inp2.js
+++ counter만들기/InputSample_inp2.js
@@ -1,12 +1,13 @@
-import React,{useState} from 'react';
+import React,{useState,useRef} from 'react';
 
-function InputSample(){
+function CounterTest(){
 
     const [inputs,setInputs] = useState({
         inpname : '',
         nickname : '',
     });
     const {inpname,nickname} = inputs;
+    const nameInput = useRef();
 
     const onChange = (e)=>{
         const {name , value} = e.target // e.target.name
@@ -29,11 +30,12 @@ function InputSample(){
             inpname : '',
             nickname : '',
         });
+        nameInput.current.focus(); //초기화시 포커스 이동(current 는 해당 DOM 선택 )
     }
 
     return(
         <div>
-            <input name="inpname" type="text" onChange={onChange} placeholder="이름"  value={inpname} />
+            <input name="inpname" type="text" onChange={onChange} placeholder="이름" ref={nameInput}  value={inpname} />
             <input name="nickname" type="text" onChange={onChange} placeholder="닉네임" value={nickname} />
             <button onClick={onReset}>초기화</button>
             <p>값 : {inpname} ({nickname})</p>
@@ -42,4 +44,4 @@ function InputSample(){
 
 }
 
-export default InputSample;
+export default CounterTest;
