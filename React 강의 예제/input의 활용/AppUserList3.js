3802800f9d781265372f9f3a460f03888ed1a7de
K_bora14 <36124197+kwakbora@users.noreply.github.com>
2021-01-12 14:30:48 +0900

    배열항목 추가,제거,수정

--- React 강의 예제/input의 활용/AppUserList3.js
+++ React 강의 예제/input의 활용/AppUserList3.js
@@ -47,28 +47,30 @@ const onCreate = ()=>{
     id : nextId.current,
     ...inputs
   }
-  //방법1  setUsers({...users,user}); //기존항목 유지하면서 추가한다
+  //방법1  setUsers({...users,user}); //기존항목 유지하면서 새로운 배열 추가
   //방법2
   setUsers(users.concat(user)); //배열의 합친다 (push를 사용하면 업데이트가 되지 않아 concat을 사용한다)
   setInputs({
     username:'',
     email:''
   });
-  nextId.current += 1; //4다음 5로 표현됨
+    console.log(nextId.current); //4 현재 값 추출
+    nextId.current += 1 //값을 바꾸고 싶을때 (값이 바뀐다고 컴포넌트가 리랜더링 되진 않는다) 알기위함
 
 } //이벤트를 호출한다고 가정
 
 const onRemove = id =>{
-  setUsers(users.filter(ss => ss.id !== id) ); //filter는 false 가 성립되었을때 추출한다.
-  // 파라미터로 가져온 id 랑 비교한다.
+  setUsers(users.filter(ss => ss.id !== id) );
+ //특정 조건을 만족하는 값들만 따로 추출하여 새로운 배열을 만든다. id 값과 같지 않은 배열만 빼고 나머지를 추출한다.
   console.log(id);
 };
 
 const onToggle = id =>{
     setUsers(users.map(
       dd => dd.id === id 
-      ? {...dd, active: !dd.active}
+      ? {...dd, active: !dd.active} //active 가 false가 됨 (true 기준)
       : dd
+      // 전체 배열을 업데이트 하면서 새로운 배열을 만든다.
     ));
 }
 
